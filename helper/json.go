36dca787b38899770d675a4e5411de59251b445a
gunawan <deddygunawan98@gmail.com>
2024-08-07 22:43:01 +0700

    just change param type data

--- helper/json.go
+++ helper/json.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 )
 
-func ReadFromRequestBody(request *http.Request, result any) {
+func ReadFromRequestBody(request *http.Request, result interface{}) {
 	decoder := json.NewDecoder(request.Body)
 	err := decoder.Decode(result)
 	PanicIfError(err)
 }
 
-func WriteToResponseBody(writer http.ResponseWriter, response any) {
-	writer.Header().Add("Content-Type", "application/json")
+func WriteToResponseBody(writer http.ResponseWriter, response interface{}) {
+	writer.Header().Set("Content-Type", "application/json")
 	encoder := json.NewEncoder(writer)
 	err := encoder.Encode(response)
 	PanicIfError(err)
