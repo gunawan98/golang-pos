3ba27b46c2f4b9482a68196308487680a4380f16
gunawan <deddygunawan98@gmail.com>
2024-08-09 15:48:30 +0700

    create login_request file

--- model/web/login_request.go
+++ model/web/login_request.go
@@ -1,6 +1,6 @@
 package web
 
 type LoginRequest struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `validate:"required,max=50,min=1" json:"username"`
+	Password string `validate:"required,max=100,min=1" json:"password"`
 }
