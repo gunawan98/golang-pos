2d3947002e4d6694198fa013a7255a5aa91bd4e8
gunawan <deddygunawan98@gmail.com>
2024-08-12 00:19:49 +0700

    create refresh token request

--- model/web/login_request.go
+++ model/web/login_request.go
@@ -4,3 +4,7 @@ type LoginRequest struct {
 	Username string `validate:"required,max=50,min=1" json:"username"`
 	Password string `validate:"required,max=100,min=1" json:"password"`
 }
+
+type RefreshTokenRequest struct {
+	Refresh string `validate:"required" json:"refresh"`
+}
