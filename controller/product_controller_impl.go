5c6940120c589b4b832d17a7766795f313ad039c
gunawan <deddygunawan98@gmail.com>
2024-08-19 20:25:03 +0700

    product controller

---
+++ controller/product_controller_impl.go
@@ -0,0 +1,95 @@
+package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gunawan98/golang-restfull-api/helper"
+	"github.com/gunawan98/golang-restfull-api/model/web"
+	"github.com/gunawan98/golang-restfull-api/service"
+	"github.com/julienschmidt/httprouter"
+)
+
+type ProductControllerImpl struct {
+	ProductService service.ProductService
+}
+
+func NewProductController(productService service.ProductService) ProductController {
+	return &ProductControllerImpl{
+		ProductService: productService,
+	}
+}
+
+func (controller *ProductControllerImpl) Create(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	productCreateRequest := web.ProductCreateRequest{}
+	helper.ReadFromRequestBody(request, &productCreateRequest)
+
+	productResponse := controller.ProductService.Create(request.Context(), productCreateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   productResponse,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller *ProductControllerImpl) Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	productUpdateRequest := web.ProductUpdateRequest{}
+	helper.ReadFromRequestBody(request, &productUpdateRequest)
+
+	productId := params.ByName("productId")
+	id, err := strconv.Atoi(productId)
+	helper.PanicIfError(err)
+
+	productUpdateRequest.Id = id
+
+	productResponse := controller.ProductService.Update(request.Context(), productUpdateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   productResponse,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller *ProductControllerImpl) Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	productId := params.ByName("productId")
+	id, err := strconv.Atoi(productId)
+	helper.PanicIfError(err)
+
+	controller.ProductService.Delete(request.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller *ProductControllerImpl) FindById(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	productId := params.ByName("productId")
+	id, err := strconv.Atoi(productId)
+	helper.PanicIfError(err)
+
+	productResponse := controller.ProductService.FindById(request.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   productResponse,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller *ProductControllerImpl) FindAll(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	productResponses := controller.ProductService.FindAll(request.Context())
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   productResponses,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
