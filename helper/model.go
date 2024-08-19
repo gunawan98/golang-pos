96a528b0499159c021d9e94cd637a4e96f126126
gunawan <deddygunawan98@gmail.com>
2024-08-19 20:24:43 +0700

    add responses for product and cart

--- helper/model.go
+++ helper/model.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gunawan98/golang-restfull-api/model/web"
 )
 
+// PRODUCT CATEGORY RESPONSE ###
 func ToCategoryResponse(category domain.Category) web.CategoryResponse {
 	return web.CategoryResponse{
 		Id:   category.Id,
@@ -21,6 +22,28 @@ func ToCategoryResponses(categories []domain.Category) []web.CategoryResponse {
 	return categoryResponse
 }
 
+// PRODUCT RESPONSE ###
+func ToProductResponse(product domain.Product) web.ProductResponse {
+	return web.ProductResponse{
+		Id:       product.Id,
+		Name:     product.Name,
+		Barcode:  product.Barcode,
+		Stock:    product.Stock,
+		Price:    product.Price,
+		Discount: product.Discount,
+	}
+}
+
+func ToProductResponses(products []domain.Product) []web.ProductResponse {
+	var productResponse []web.ProductResponse
+	for _, product := range products {
+		productResponse = append(productResponse, ToProductResponse(product))
+	}
+
+	return productResponse
+}
+
+// USER RESPONSE ###
 func ToUserResponse(user domain.User) web.UserResponse {
 	return web.UserResponse{
 		Id:       user.Id,
@@ -36,4 +59,25 @@ func ToUserResponses(users []domain.User) []web.UserResponse {
 	}
 
 	return userResponse
+}
+
+// CART RESPONSE ###
+func ToCartResponse(cart domain.Cart) web.CartResponse {
+	return web.CartResponse{
+		Id:        cart.Id,
+		CashierID: cart.CashierID,
+		Completed: cart.Completed,
+		CreatedAt: cart.CreatedAt,
+	}
+}
+
+func ToCartItemResponse(cartItem domain.CartItem) web.CartItemResponse {
+	return web.CartItemResponse{
+		Id:         cartItem.Id,
+		CartID:     cartItem.CartID,
+		ProductID:  cartItem.ProductID,
+		Quantity:   cartItem.Quantity,
+		UnitPrice:  cartItem.UnitPrice,
+		TotalPrice: cartItem.TotalPrice,
+	}
 }
