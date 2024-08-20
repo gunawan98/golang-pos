7fa58c8bdf5312319886c6f5edbc1bed2f101c56
gunawan <deddygunawan98@gmail.com>
2024-08-20 21:28:37 +0700

    make purchase validation request

---
+++ model/web/purchase_request_response.go
@@ -0,0 +1,19 @@
+package web
+
+import "time"
+
+type PurchaseCreateRequest struct {
+	CartID        int       `validate:"required" json:"cart_id"`
+	CashierID     int       `validate:"required" json:"cashier_id"`
+	TotalAmount   int       `validate:"required" json:"total_amount"`
+	PaymentMethod string    `json:"payment_method"`
+	CreatedAt     time.Time `json:"created_at"`
+}
+
+type PurchaseResponse struct {
+	CartID        int       `json:"cart_id"`
+	CashierID     int       `json:"cashier_id"`
+	TotalAmount   int       `json:"total_amount"`
+	PaymentMethod string    `json:"payment_method"`
+	CreatedAt     time.Time `json:"created_at"`
+}
