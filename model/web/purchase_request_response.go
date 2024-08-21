8994b5b19f0841bb56cc9608fc015b0edfed07eb
gunawan <deddygunawan98@gmail.com>
2024-08-22 01:04:35 +0700

    remove required validate

--- model/web/purchase_request_response.go
+++ model/web/purchase_request_response.go
@@ -6,7 +6,7 @@ type PurchaseCreateRequest struct {
 	CartID        int       `validate:"required" json:"cart_id"`
 	CashierID     int       `validate:"required" json:"cashier_id"`
 	TotalAmount   int       `validate:"required" json:"total_amount"`
-	PaymentMethod string    `json:"payment_method"`
+	PaymentMethod string    `validate:"oneof=cash credit-card ewallet other" json:"payment_method"`
 	CreatedAt     time.Time `json:"created_at"`
 }
 
