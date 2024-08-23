3575612d1a7d258d8065ace1e503edbb66b64418
gunawan <deddygunawan98@gmail.com>
2024-08-23 21:20:30 +0700

    slicing route from main

--- main.go
+++ main.go
@@ -7,12 +7,10 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gunawan98/golang-restfull-api/app"
 	"github.com/gunawan98/golang-restfull-api/controller"
-	"github.com/gunawan98/golang-restfull-api/exception"
 	"github.com/gunawan98/golang-restfull-api/helper"
 	"github.com/gunawan98/golang-restfull-api/middleware"
 	"github.com/gunawan98/golang-restfull-api/repository"
 	"github.com/gunawan98/golang-restfull-api/service"
-	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -42,36 +40,7 @@ func main() {
 
 	loginController := controller.NewLoginController(userService)
 
-	router := httprouter.New()
-
-	router.POST("/api/login", loginController.Login)
-	router.POST("/api/refresh", loginController.Refresh)
-
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
-
-	router.GET("/api/product", productController.FindAll)
-	router.GET("/api/product/:productId", productController.FindById)
-	router.POST("/api/product", productController.Create)
-	router.PUT("/api/product/:productId", productController.Update)
-	router.DELETE("/api/product/:productId", productController.Delete)
-
-	router.GET("/api/user", userController.FindAll)
-	router.GET("/api/user/:userId", userController.FindById)
-	router.POST("/api/user", userController.Create)
-	router.PUT("/api/user/:userId", userController.Update)
-	router.DELETE("/api/user/:userId", userController.Delete)
-
-	router.POST("/api/cart", cartController.CreateCart)
-	router.POST("/api/cart-item/:cartId", cartController.AddItem)
-	router.GET("/api/cart-item/:cartId", cartController.GetCartDetails)
-
-	router.POST("/api/purchase", purchaseController.ConfirmPayment)
-
-	router.PanicHandler = exception.ErrorHandler
+	router := app.NewRouter(loginController, categoryController, productController, userController, cartController, purchaseController)
 
 	// Protect routes with the middleware
 	protectedRouter := middleware.NewAuthMiddleware(router)
